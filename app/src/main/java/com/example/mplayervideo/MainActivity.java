725a36118b6a3a5b1fcf0cfa02d66a4e99dd869c
Hieu <thanhhieu995@gmail.com>
2022-01-19 14:13:29 +0700

    Add ListView to .xml

--- app/src/main/java/com/example/mplayervideo/MainActivity.java
+++ app/src/main/java/com/example/mplayervideo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -14,10 +15,16 @@ import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    String[] items;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        runtimePermission();
     }
 
     public void runtimePermission() {
@@ -37,6 +44,6 @@ public class MainActivity extends AppCompatActivity {
                     public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
 
                     }
-                });
+                }).check();
     }
 }
